86fd40a819e2b84a9ed39e3124f276d293126a5a
Dmitry Kovalenko <dmitry.lipetsk@gmail.com>
2016-01-13 15:53:00 +0300

    formatting

--- src/dsql/make.cpp
+++ src/dsql/make.cpp
@@ -1534,7 +1534,8 @@ void MAKE_desc_from_field(dsc* desc, const dsql_fld* field)
     {
 		INTL_ASSIGN_DSC(desc, field->fld_character_set_id, field->fld_collation_id);
 	}
-	else if (desc->dsc_dtype == dtype_blob)
+	else
+    if (desc->dsc_dtype == dtype_blob)
 	{
 		desc->dsc_scale = static_cast<SCHAR>(field->fld_character_set_id);
 		desc->dsc_flags |= field->fld_collation_id << 8;
